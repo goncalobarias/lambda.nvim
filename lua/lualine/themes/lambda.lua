f064415fbb40bf62e958794d8716789fc29da57c
Gocho <goncalo.barias@gmail.com>
2024-03-26 11:45:24 +0000

    Update the lambda palette

--- lua/lualine/themes/lambda.lua
+++ lua/lualine/themes/lambda.lua
@@ -1,46 +1,47 @@
 -- Lambda lualine theme
 local colors = {
-  gray       = '#040913',
+  gray       = '#181a23',
   lightgray  = '#4d4d4d',
   orange     = '#ffb86c',
-  purple     = '#4457a2',
-  blue       = '#106295',
-  red        = '#aa1872',
+  purple     = '#bd93f9',
+  accent     = '#604c7e',
+  blue       = '#0189cc',
+  red        = '#ff5555',
   yellow     = '#f1fa8c',
-  green      = '#079374',
+  green      = '#50fa7b',
   white      = '#f8f8f2',
   black      = '#000000',
 }
 
 return {
   normal = {
-    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
   insert = {
     a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
   visual = {
     a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
   replace = {
-    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
   command = {
-    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
   inactive = {
     a = { bg = colors.lightgray, fg = colors.white, gui = 'bold' },
-    b = { bg = colors.blue, fg = colors.white },
+    b = { bg = colors.accent, fg = colors.white },
     c = { bg = colors.gray, fg = colors.white },
   },
 }
